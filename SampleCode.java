3e13cd3f5b095013456f62a7a9eb76daf36e713b
stuti30 <84438223+stuti30@users.noreply.github.com>
2021-06-16 05:07:11 +0000

    Update SampleCode.java

--- SampleCode.java
+++ SampleCode.java
@@ -1,5 +1,6 @@
 public class SampleCode {
   public static void main(String[] args) {
     System.out.println("Hello World version2.0");
+     System.out.println("Changes pulled from Branch 2");
   }
 }
